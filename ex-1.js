689ce72549d0fe01c13dff4f7cc544e51d655f12
panatyuki <admiral.dancho@gmail.com>
2023-11-28 22:17:15 +0700

    feat: add complete exercise

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,31 @@
-// Start coding here
+// 1
+function add(a, b) {
+  return a + b;
+}
+
+let answer1 = add(10, 5);
+console.log(answer1);
+
+// 2
+function subtract(a, b) {
+  return a - b;
+}
+
+let answer2 = subtract(10, 5);
+console.log(answer2);
+
+// 3
+function multiply(a, b) {
+  return a * b;
+}
+
+let answer3 = multiply(10, 5);
+console.log(answer3);
+
+// 4
+function divide(a, b) {
+  return a / b;
+}
+
+let answer4 = divide(10, 5);
+console.log(answer4);
